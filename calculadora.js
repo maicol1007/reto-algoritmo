cb6cbc8a7f9ce47fc767eed5ec425bafce05b45e
Avpa <juanxan123@gmail.com>
2022-06-28 17:20:31 -0500

    algoritmo de suma prompt

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+const operando1 = document.getElementById("num1");
+const operando2 = document.getElementById("num2");
+
+function sumar (){
+ let a =parseFloat(operando1.value) ;
+ let b =parseFloat(operando2.value) ;
+alert(a+b);
+}
+
+
